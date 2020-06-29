3949621565d0e0e9283ef386cf410fbbf10b5b48
Кирилл Брагин <mr.kirya.bragin@mail.ru>
2020-06-29 09:45:16 +0300

    commit client ui + code #1

---
+++ ITDevelopment_Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITDevelopment_Project
+{
+    static class Program
+    {
+        public static ITDevelopmentEntities2 itDb = new ITDevelopmentEntities2();
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormAuthorization());
+        }
+    }
+}
